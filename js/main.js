d0bafcfb0ace86d7bcfa73c527df0622ee53b646
32Fire32 <nicola.gianfelici@hotmail.it>
2022-11-03 22:47:49 +0100

    aggiunge reload della pagina dopo sconfitta

--- js/main.js
+++ js/main.js
@@ -2,37 +2,37 @@
 
 // FUNZIONI
 
-function startGame(board, maxNumbers, level){
-        cellContainer.innerHTML ="";
-        let cellNumber;
+function startGame(board, cells, maxNumbers, level){
+        board.innerHTML ="";
         for ( let i = 1; i <=maxNumbers; i++){
-            cellNumber = document.createElement("div");
-            cellNumber.innerHTML = i;
-            cellNumber.classList.add("cellboard");
-            cellNumber.addEventListener('click', function(){
+            cells = document.createElement("div");
+            cells.innerHTML = i;
+            cells.classList.add("cellboard");
+            cells.addEventListener('click', function(){
                 scoreNumber.push(i);
                 document.getElementById("score").innerHTML = scoreNumber.length;
                 this.classList.add("active");
                 })
                 if (level === "easy") {
-                    cellNumber.classList.add("easy");
+                    cells.classList.add("easy");
                 } else if (level === "hard") {
-                    cellNumber.classList.add("hard");
+                    cells.classList.add("hard");
                 } else if (level === "hell") {
-                    cellNumber.classList.add("hell");
+                    cells.classList.add("hell");
                 }
-                // console.log(Number(cellNumber.innerHTML));
-                board.append(cellNumber);
-                endGame(cellNumber, bombArray, randomNumber);
+                // console.log(Number(cells.innerHTML));
+                board.append(cells);
+                endGame(cells, bombArray);
         }        
 }
 
-function endGame(cell, array, number) {
+function endGame(cell, array) {
     cell.addEventListener('click', function(){
         if (array.indexOf(Number(cell.innerHTML)) != -1){
             cell.classList.add("bomb");
             console.log(cell.innerHTML);
-            alert("hai perso");
+            document.getElementById("finalscore").innerHTML = "Mi dispiace hai perso! Il tuo punteggio è " + (scoreNumber.length - 1);
+            document.getElementById("reload").innerHTML = "Clicca QUI per riavviare";
         } else {
             console.log("nessuna bomba");
         }
@@ -42,17 +42,17 @@ function endGame(cell, array, number) {
 
 // creo la funzione delle bombe con 16 numeri univoci
 
-function createBombs(number){
-    while ( bombArray.length < 16 ){
+function createBombs(array, random, randomarray, number){
+    while ( array.length < 16 ){
         i++;
-        randomNumber = (Math.floor(Math.random() * number) + 1);
-        randomNumberarray.push(randomNumber);
-        if (bombArray.includes(randomNumber) === false ){
-            bombArray.push(randomNumber);
+        random = (Math.floor(Math.random() * number) + 1);
+        randomarray.push(random);
+        if (array.includes(random) === false ){
+            array.push(random);
             // console.log(randomNumber);
         }
     }
-    console.log("l'array delle bombe è " + bombArray);
+    console.log("l'array delle bombe è " + array);
 }
 
 
@@ -62,27 +62,37 @@ const playButton = document.querySelector("button");
 // mi prendo la board
 const cellContainer = document.querySelector(".board");
 
+//mi prendo il tasto reload
+
+const reloadButton = document.getElementById("reload");
+
 let i = 1;
 let randomNumber = 0;
 let randomNumberarray = [];
 let bombArray = [];
 let scoreNumber= [];
+let cellNumber;
 
 // mi prendo i valori delle options
 playButton.addEventListener('click', function(){
     const selectElement = document.getElementById("level").value;
     console.log(selectElement);
     if ( selectElement === "easy") {        
-        startGame(cellContainer, 100, "easy");   
-        createBombs(100);     
+        startGame(cellContainer, cellNumber, 100, "easy");   
+        createBombs(bombArray, randomNumber, randomNumberarray, 100);     
     } else if (selectElement === "hard") {        
-        startGame(cellContainer, 81, "hard");  
-        createBombs(81);           
+        startGame(cellContainer, cellNumber, 81, "hard");  
+        createBombs(bombArray, randomNumber, randomNumberarray, 81);           
     } else {        
-        startGame(cellContainer, 49, "hell");
-        createBombs(49); 
+        startGame(cellContainer, cellNumber, 49, "hell");
+        createBombs(bombArray, randomNumber, randomNumberarray, 49); 
     }
     
+})
+
+//reload della pagina
+reloadButton.addEventListener('click', function(){
+    window.location.reload();
 })
 
 
