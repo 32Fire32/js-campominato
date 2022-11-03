0e3382e019023276c35877981a05e08d443a0e40
32Fire32 <nicola.gianfelici@hotmail.it>
2022-11-03 14:08:13 +0100

    array bombe con numeri univoci

--- js/main.js
+++ js/main.js
@@ -9,7 +9,7 @@ function startGame(board, maxNumbers, level){
             cellNumber.innerHTML = i;
             cellNumber.classList.add("cellboard");
             cellNumber.addEventListener('click', function(){
-                cellNumber.classList.add("active");
+                this.classList.add("active");
                 console.log(i);
             })
             if (level === "easy") {
@@ -23,11 +23,32 @@ function startGame(board, maxNumbers, level){
         }
 }
 
+//mi prendo lo score
+const score = document.getElementById("score");
+
 //mi prendo il tasto play
 const playButton = document.querySelector("button");
 
 // mi prendo la board
 const cellContainer = document.querySelector(".board");
+
+// creo l'array delle bombe con 16 numeri univoci
+
+let i = 0;
+let randomNumberarray = [];
+let bombArray = [];
+while ( bombArray.length < 16 ){
+    i++;
+    let randomNumber = (Math.floor(Math.random() * 16) + 1);
+    console.log("il numero random è " + randomNumber);
+    randomNumberarray.push(randomNumber);
+    console.log("l'array dei numero randomici è " + randomNumberarray);
+    if (bombArray.includes(randomNumber) === false ){
+        bombArray.push(randomNumber);
+    }
+
+}
+console.log("l'array delle bombe è " + bombArray);
 
 // mi prendo i valori delle options
 playButton.addEventListener('click', function(){
@@ -42,3 +63,5 @@ playButton.addEventListener('click', function(){
     }
     
 })
+
+
