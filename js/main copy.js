a3a36170f692e99699db37ea1fe63b8e8b362a40
32Fire32 <nicola.gianfelici@hotmail.it>
2022-11-04 17:21:26 +0100

    miglioramento e pulizia codice dopo lezione

--- js/main copy.js
+++ js/main copy.js
@@ -9,40 +9,31 @@ function startGame(board, maxNumbers, level){
             cellNumber = document.createElement("div");
             cellNumber.innerHTML = i;
             cellNumber.classList.add("cellboard");
-            cellNumber.addEventListener('click', work);
-            function work(){
-                
-                }
-                if (level === "easy") {
-                    cellNumber.classList.add("easy");
-                } else if (level === "hard") {
-                    cellNumber.classList.add("hard");
-                } else if (level === "hell") {
-                    cellNumber.classList.add("hell");
-                }
-                // console.log(Number(cellNumber.innerHTML));
+            cellNumber.classList.add(level);
             board.append(cellNumber);
-            endGame(cellNumber, bombArray);
+            Game(cellNumber, bombArray);
         }        
-}
+    }
 
-function endGame(cell, array) {
-    cell.addEventListener('click', on);
-    function on(){
+function Game(cell, array) {
+    cell.addEventListener('click', function(){
+        if(gameOver){
+            return
+        }
         if (array.indexOf(Number(cell.innerHTML)) != -1){
             cell.classList.add("bomb");
             console.log(cell.innerHTML);
-            document.getElementById("finalscore").innerHTML = "Mi dispiace hai perso! Il tuo punteggio è " + (scoreNumber.length - 1);
+            document.getElementById("finalscore").innerHTML = "Mi dispiace hai perso! Il tuo punteggio è " + scoreNumber.length;
             document.getElementById("reload").innerHTML = "Clicca QUI per riavviare";
-            cell.removeEventListener('click', work());
             console.log(scoreNumber);
+            gameOver = true;
         } else {
             scoreNumber.push(i);
             document.getElementById("score").innerHTML = scoreNumber.length;
             this.classList.add("active");
         }
-        console.log(cell.innerHTML, array.indexOf(Number(cell.innerHTML)), typeof(Number(cell.innerHTML)))
-    };
+        // console.log(cell.innerHTML, array.indexOf(Number(cell.innerHTML)), typeof(Number(cell.innerHTML)))
+    });
 }
 
 // creo la funzione delle bombe con 16 numeri univoci
@@ -64,6 +55,7 @@ function createBombs(array, random, randomarray, number){
 /////// MAIN/////////
 /////////////////////
 
+
 //mi prendo il tasto play
 const playButton = document.querySelector("button");
 
@@ -78,21 +70,26 @@ let randomNumber = 0;
 let randomNumberarray = [];
 let bombArray = [];
 let scoreNumber= [];
+let TotalCellTabs;
+let gameOver = false;
 
 
 // mi prendo i valori delle options
 playButton.addEventListener('click', function(){
     const selectElement = document.getElementById("level").value;
     console.log(selectElement);
-    if ( selectElement === "easy") {        
-        startGame(cellContainer, 100, "easy");   
-        createBombs(bombArray, randomNumber, randomNumberarray, 100);     
-    } else if (selectElement === "hard") {        
-        startGame(cellContainer, 81, "hard");  
+    if ( selectElement === "easy") { 
+        let TotalCellTabs = 100;   
+        createBombs(bombArray, randomNumber, randomNumberarray, 100);    
+        startGame(cellContainer, TotalCellTabs, "easy");   
+    } else if (selectElement === "hard") {  
+        let TotalCellTabs = 81;    
         createBombs(bombArray, randomNumber, randomNumberarray, 81);           
+        startGame(cellContainer, TotalCellTabs, "hard");  
     } else {        
-        startGame(cellContainer, 49, "hell");
+        let TotalCellTabs = 49; 
         createBombs(bombArray, randomNumber, randomNumberarray, 49); 
+        startGame(cellContainer, TotalCellTabs, "hell");
     }
     
 })
@@ -102,6 +99,11 @@ reloadButton.addEventListener('click', function(){
     window.location.reload();
 })
 
+//vittoria
+
+if(scoreNumber.length === TotalCellTabs - 16) {
+    alert("bravissimo hai terminato il gioco");
+}
 
 
 
